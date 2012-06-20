8147bcc57658b218056ff90c36a96cbc32d6e61b
jDevEE <jdeveloper@poczta.fm>
2012-06-20 23:35:56 +0200

    Added Git Utility class

--- com/learning/git/HelloGit.java
+++ com/learning/git/HelloGit.java
@@ -1,6 +1,6 @@
 package com.learning.git;
 public class HelloGit{
 	public static void main(String ... args ){
-		System.out.print("Hello world, git test");
+		HelloGitUtil.sayHello("Git");
 	}
 }
---
+++ com/learning/git/HelloGitUtil.java
@@ -0,0 +1,9 @@
+package com.learning.git;
+public final class HelloGitUtil{
+	/** Default constructor for utility classes. **/
+	private HelloGitUtil(){
+	}
+	public static void sayHello(String name){
+		System.out.println("Hello " + name);
+	}
+}
